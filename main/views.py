d3fcb5fac4164ef0bc99aac3df636826685d4ee9
DAPotapov <103317598+DAPotapov@users.noreply.github.com>
2024-02-09 14:56:08 +0300

    Reformatted with black

--- main/views.py
+++ main/views.py
@@ -6,29 +6,27 @@ from .models import Question, Choice, Survey, UsersActivity
 
 # Create your views here.
 def index(request):
-    """ Стартовая страница приложения показывает перечень имеющихся опросов """
+    """Стартовая страница приложения показывает перечень имеющихся опросов"""
     # В задании указано требование "Без использования ORM" только перед требованиями к
     # результатам опросов. Следовательно, в остальных местах допускается использование ORM.
     surveys = Survey.objects.all()
 
-    # Каждый запрос на главную страницу создаёт новую сессию, удобно для заполнения таблицы с результатами
+    # Каждый запрос на главную страницу создаёт новую сессию, 
+    # удобно для заполнения таблицы с результатами
     # request.session.create()
-    content = {
-        'header': "Доступные опросы",
-        'surveys': surveys
-    }
-    return render(request, 'main/index.html', content)
+    content = {"header": "Доступные опросы", "surveys": surveys}
+    return render(request, "main/index.html", content)
 
 
 def treat_survey(request):
     """
     Функция обрабатывает выбор опроса пользователем
-    и перенаправляет на страницу с вопросами 
+    и перенаправляет на страницу с вопросами
     """
 
     if request.method == "POST":
         # Заготавливаем данные для сообщения об ошибке
-        err_code = '404'
+        err_code = "404"
         error = "Упс! Что-то пошло не так..."
 
         # Получаем идентификатор выбранного опроса
@@ -36,108 +34,128 @@ def treat_survey(request):
         survey = Survey.objects.get(pk=survey_id)
 
         # Запоминаем выбранный опрос в сессии
-        request.session['survey'] = {
-            'id': survey.id,
-            'text': survey.text,
-            'description': survey.description
+        request.session["survey"] = {
+            "id": survey.id,
+            "text": survey.text,
+            "description": survey.description,
         }
 
         # Задаем первый вопрос пользователю с вариантами ответа
         # или перенаправляем на страницу с ошибкой, если что-то не так
         try:
-            question = Question.objects.get(survey__id=survey_id, parent_question__isnull=True)
+            question = Question.objects.get(
+                survey__id=survey_id, parent_question__isnull=True
+            )
         except (Question.DoesNotExist, Question.MultipleObjectsReturned):
-            return render(request, 'main/error.html', {'err_code': err_code, 'error': error})
-        else:        
+            return render(
+                request, "main/error.html", {"err_code": err_code, "error": error}
+            )
+        else:
             choices = Choice.objects.filter(question__id=question.id)
             if choices.exists():
                 context = {
-                    'header': "Опрос: " + survey_id,
-                    'question': question,
-                    'choices': choices
-                    }
-                return render(request, 'main/questions.html', context)
+                    "header": "Опрос: " + survey_id,
+                    "question": question,
+                    "choices": choices,
+                }
+                return render(request, "main/questions.html", context)
             else:
-                return render(request, 'main/error.html', {'err_code': err_code, 'error': error})
-            
+                return render(
+                    request, "main/error.html", {"err_code": err_code, "error": error}
+                )
+
     # Отправляем пользователя на главную страницу, если он попытался зайти по прямой ссылке
     else:
         return redirect("/")
 
 
 def treat_answer(request):
-    """ 
+    """
     Функция запоминает ответы пользователя на вопрос и
     показывает следующий вопрос.
     После окончания опроса перенаправляет на страницу с результатами
     """
 
     if request.method == "POST":
-        err_code = '404'
+        err_code = "404"
         error = "Упс! Что-то пошло не так..."
 
-        survey = request.session.get('survey', '')
+        survey = request.session.get("survey", "")
 
         # Получаем информацию о вопросе и ответе на него из формы
         try:
-            question_id, choosen_id = request.POST.get("choice").split('_')
+            question_id, choosen_id = request.POST.get("choice").split("_")
         except AttributeError:
-            return render(request, 'main/error.html', {'err_code': err_code, 'error': error})
-        
+            return render(
+                request, "main/error.html", {"err_code": err_code, "error": error}
+            )
+
         # Записываем полученную информацию в БД
         try:
             question = Question.objects.get(pk=question_id)
             choice = Choice.objects.get(pk=choosen_id)
             answer = UsersActivity(
-                user_id=request.session.session_key,
-                question=question,
-                choice=choice
-                )
+                user_id=request.session.session_key, question=question, choice=choice
+            )
             answer.save()
         except (
             Question.DoesNotExist,
             Question.MultipleObjectsReturned,
             Choice.DoesNotExist,
-            Choice.MultipleObjectsReturned):
-            return render(request, 'main/error.html', {'err_code': err_code, 'error': error})
+            Choice.MultipleObjectsReturned,
+        ):
+            return render(
+                request, "main/error.html", {"err_code": err_code, "error": error}
+            )
         except DataError:
-            err_code = '500'
-            error = 'Не удалось записать в базу данных. Попробуйте позже.'
-            return render(request, 'main/error.html', {'err_code': err_code, 'error': error})
+            err_code = "500"
+            error = "Не удалось записать в базу данных. Попробуйте позже."
+            return render(
+                request, "main/error.html", {"err_code": err_code, "error": error}
+            )
         else:
-
             # Если ответ никуда не ведёт, значит опрос закончен и пора показывать результаты
             if not choice.next_question:
                 stats = []
                 try:
-                    survey_results = get_results(request.session.get('survey', '')['id'])
-                    survey_results['title'] = survey['text']
+                    survey_results = get_results(
+                        request.session.get("survey", "")["id"]
+                    )
+                    survey_results["title"] = survey["text"]
                 except (AttributeError, ValueError, IndexError, TypeError):
-                    return render(request, 'main/error.html', {'err_code': err_code, 'error': error})
+                    return render(
+                        request,
+                        "main/error.html",
+                        {"err_code": err_code, "error": error},
+                    )
                 else:
                     stats.append(survey_results)
-                    context = {
-                        'stats': stats
-                    }
-                    return render(request, 'main/statistics.html', context=context)
+                    context = {"stats": stats}
+                    return render(request, "main/statistics.html", context=context)
 
             # Готовим следующий вопрос и варианты ответа на него
             try:
                 question = choice.next_question
             except (Question.DoesNotExist, Question.MultipleObjectsReturned):
-                return render(request, 'main/error.html', {'err_code': err_code, 'error': error})
+                return render(
+                    request, "main/error.html", {"err_code": err_code, "error": error}
+                )
             else:
                 choices = question.choices.all()
                 if choices.exists():
                     context = {
-                        'header': survey,
-                        'question': question,
-                        'choices': choices
-                        }
-                    return render(request, 'main/questions.html', context)
+                        "header": survey,
+                        "question": question,
+                        "choices": choices,
+                    }
+                    return render(request, "main/questions.html", context)
                 else:
-                    return render(request, 'main/error.html', {'err_code': err_code, 'error': error})
-                
+                    return render(
+                        request,
+                        "main/error.html",
+                        {"err_code": err_code, "error": error},
+                    )
+
     else:
         return redirect("/")
 
@@ -149,11 +167,10 @@ def get_results(survey_id):
     """
 
     # Подключаемся к БД
-    db_name = 'default'
+    db_name = "default"
     conn = connections[db_name]
     survey_stats = {}
     with conn.cursor() as cursor:
-        
         # Количество участников опроса
         query = "SELECT COUNT(DISTINCT user_id) FROM main_usersactivity WHERE question_id IN (SELECT id FROM main_question WHERE survey_id=%s)"
         cursor.execute(query, [survey_id])
@@ -212,27 +229,31 @@ def get_results(survey_id):
         """
         cursor.execute(query, [survey_id])
         choices = cursor.fetchall()
-            
+
         # Объединим полученные данные, преобразовав в словарь
-        c_keys = ['text', 'question_id', 'choice_count']
+        c_keys = ["text", "question_id", "choice_count"]
         new_questions = []
-        q_keys = ['id', 'text', 'record_count', 'position']
+        q_keys = ["id", "text", "record_count", "position"]
         for q in questions:
             question = dict(zip(q_keys, q))
             q_choices = []
             for c in choices:
                 choice_dict = dict(zip(c_keys, c))
-                if choice_dict['question_id'] == question['id']:
-                    choice_dict['rate'] = choice_dict['choice_count'] / question['record_count'] * 100  # процент
+                if choice_dict["question_id"] == question["id"]:
+                    choice_dict["rate"] = (
+                        choice_dict["choice_count"] / question["record_count"] * 100
+                    )  # процент
                     q_choices.append(choice_dict)
-            question['choices'] = q_choices
-            question['rate'] = question['record_count'] / respondent_count * 100  # процент
+            question["choices"] = q_choices
+            question["rate"] = (
+                question["record_count"] / respondent_count * 100
+            )  # процент
             new_questions.append(question)
 
         # Соберём словарь для передачи в вызывающую функции, а затем в шаблон
         survey_stats = {
-            'respondent_count': respondent_count,
-            'questions': new_questions
+            "respondent_count": respondent_count,
+            "questions": new_questions,
         }
 
     return survey_stats
@@ -245,7 +266,11 @@ def statistics(request):
 
     surveys_count = Survey.objects.count()
     if not surveys_count:
-        return render(request, 'main/error.html', {'err_code': '404', 'error': "Надо же... ни одного опроса нет ещё..."})
+        return render(
+            request,
+            "main/error.html",
+            {"err_code": "404", "error": "Надо же... ни одного опроса нет ещё..."},
+        )
     stats = []
     surveys = Survey.objects.all()
 
@@ -254,10 +279,12 @@ def statistics(request):
         try:
             survey_stats = get_results(survey.id)
         except (IndexError, TypeError):
-            return render(request, 'main/error.html', {'err_code': '404', 'error': "Не удалось получить результаты опроса"})
-        survey_stats['title'] = survey.text
+            return render(
+                request,
+                "main/error.html",
+                {"err_code": "404", "error": "Не удалось получить результаты опроса"},
+            )
+        survey_stats["title"] = survey.text
         stats.append(survey_stats)
-    context = {
-        'stats': stats
-    }
-    return render(request, 'main/statistics.html', context=context)
+    context = {"stats": stats}
+    return render(request, "main/statistics.html", context=context)
