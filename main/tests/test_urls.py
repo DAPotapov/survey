19907e08068df34a955c12f0e7612360b58d0579
DAPotapov <103317598+DAPotapov@users.noreply.github.com>
2024-02-14 13:36:39 +0300

    URLs tests added for /admin and /stats

--- main/tests/test_urls.py
+++ main/tests/test_urls.py
@@ -2,6 +2,18 @@ from django.test import TestCase
 
 
 class URLTests(TestCase):
+    """
+    Тест URL-адресов приложения, которые можно посетить. 
+    Остальные используют редирект для GET-запросов и тестируются в test_views.py
+    """
     def test_homepage(self):
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
+
+    def test_statistics(self):
+        response = self.client.get('/stats/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_admin(self):
+        response = self.client.get('/admin/')
+        self.assertEqual(response.status_code, 302)
--- main/tests/test_views.py
+++ main/tests/test_views.py
@@ -1,12 +1,21 @@
 from django.test import TestCase
 from main.views import index, treat_survey, treat_answer, statistics
+from main.models import Survey, Question, Choice, UsersActivity
 
 
-class TestViews(TestCase):
+class TestIndexView(TestCase):
     def test_index(self):
         response = index(self)
         self.assertEqual(response.status_code, 200)
 
+    def test_no_surveys(self):
+        Survey.objects.all().delete()
+        response = index(self)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.context["surveys"], [])
+
+
+class TestStatisticsView(TestCase):
     def test_statistics(self):
         response = statistics(self)
         self.assertEqual(response.status_code, 200)
