bcd3dfec8d76c4f204e250cb924fcbdf6b215324
DAPotapov <103317598+DAPotapov@users.noreply.github.com>
2024-02-20 10:15:20 +0300

    Moved factory classes into a separate file for re-use. Updated test_models accordingly

--- main/tests/test_models.py
+++ main/tests/test_models.py
@@ -2,41 +2,7 @@ import factory
 import uuid
 from django.test import TestCase
 from main.models import Survey, Question, Choice, UsersActivity
-
-
-class SurveyFactory(factory.Factory):
-    class Meta:
-        model = Survey
-
-    text = factory.Faker("sentence")
-    description = factory.Faker("sentence")
-
-
-class QuestionFactory(factory.Factory):
-    class Meta:
-        model = Question
-
-    text = factory.Faker("sentence")
-    description = factory.Faker("sentence")
-    survey = factory.SubFactory(SurveyFactory)
-
-
-class ChoiceFactory(factory.Factory):
-    class Meta:
-        model = Choice
-
-    text = factory.Faker("sentence")
-    question = factory.SubFactory(QuestionFactory)
-    next_question = factory.SubFactory(QuestionFactory)
-
-
-class UsersActivityFactory(factory.Factory):
-    class Meta:
-        model = UsersActivity
-
-    user_id = uuid.uuid4().hex
-    choice = factory.SubFactory(ChoiceFactory)
-    question = factory.SubFactory(QuestionFactory)
+from main.factories import SurveyFactory, QuestionFactory, ChoiceFactory, UsersActivityFactory
 
 
 class TestSurveyModel(TestCase):
