14fa29d2ffcb38219f6fb6156804fa136099cdb1
DAPotapov <103317598+DAPotapov@users.noreply.github.com>
2024-02-21 12:04:51 +0300

    Start making test for get_results

--- main/tests/test_views.py
+++ main/tests/test_views.py
@@ -2,7 +2,7 @@ import re
 import pprint
 from django.test import TestCase, Client
 from django.urls import reverse
-from main.views import index, treat_survey, treat_answer, statistics
+from main.views import index, treat_survey, treat_answer, statistics, get_results_orm
 from main.models import Survey, Question, Choice, UsersActivity
 from main.factories import SurveyFactory, QuestionFactory, ChoiceFactory, UsersActivityFactory
 
@@ -47,3 +47,12 @@ class TestStatisticsView(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, "main/error.html")
         self.assertQuerySetEqual(response.context["error"], 'Надо же... ни одного опроса нет ещё...')
+
+class TestGetResults(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.activities = UsersActivityFactory.create_batch(10)
+
+    def test_get_results(self):
+        # TODO фабрики надо переделать, один-ко-многим было реально 1 ко многим. Иначе сложно тестировать, когда 1 ответ на 1 вопрос в 1 опросе. И то не всегда.
+        print(get_results_orm(3))
